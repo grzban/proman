fd8d2e5207be32f7c49fb59ecbea2089e9420f83
Wirek90 <annawirecka@gmail.com>
2018-05-16 14:40:07 +0200

    u mnie dziala...

--- static/js/dom.js
+++ static/js/dom.js
@@ -16,34 +16,23 @@ let dom = {
             boardBox.id = "board-box-" + boards[i].id;
             boardBox.className = "container";
 
-            let singleBoardContainer = document.createElement("div");
-            singleBoardContainer.id = "board-" + boards[i].id;
-
             let singleBoard = document.createElement("div");
-            singleBoard.id = "board-" + boards[i].id + "-container";
+            singleBoard.id = "board-" + boards[i].id;
             singleBoard.className = "row";
 
             let titleButton = document.createElement("button");
             titleButton.id = "board-" + boards[i]["id"] + "-btn";
             titleButton.className = "btn-primary";
             titleButton.style.width = "100%";
-            let cardButton = document.createElement("button");
-            cardButton.className = "btn-warning";
-            cardButton.style.fontSize = "12px";
+            singleBoard.appendChild(titleButton);
 
             boardBox.appendChild(titleButton);
-            boardBox.appendChild(singleBoardContainer);
-            singleBoardContainer.appendChild(cardButton);
-            singleBoardContainer.appendChild(singleBoard);
+            boardBox.appendChild(singleBoard);
 
-            let buttonHeader = document.createElement("h3");
+            let buttonHeader = document.createElement("h4");
             let txt = document.createTextNode(boards[i]["title"]);
             buttonHeader.appendChild(txt);
             titleButton.appendChild(buttonHeader);
-            let cardButtonHeader = document.createElement("span");
-            let cardTxt = document.createTextNode("Add new Card");
-            cardButton.appendChild(cardButtonHeader);
-            cardButtonHeader.appendChild(cardTxt);
 
             let statuses = dataHandler.getStatuses();
             statuses.forEach(function(status) {
@@ -62,27 +51,23 @@ let dom = {
                 newStatus.appendChild(newStatusHeader);
                 newStatusHeader.appendChild(newStatusTitle);
                 newStatus.appendChild(cardsWindow);
+
             });
-
             boardsDiv.appendChild(boardBox);
-            
-            let board = boards[i];
-            let id = board.id;
-            let boardDiv = document.getElementById('board-'+ id);
+            let cards = dom.loadCards(boards[i].id);
+            dom.showCards(boards[i].id, cards);
 
-            if (board.is_active) {
-                boardDiv.style.display = ""
-            } else {
-                boardDiv.style.display = "none";
-            }
-            titleButton.onclick = function() {
-                dataHandler.changeStatus(id);
-                dom.loadBoards();
+            titleButton.onclick = function(e) {
+                e.preventDefault();
+                let id = e.target.id; //changed due to FF bugs
+                let board = boards[i];
+                if (board.is_active) {
+                    dom.showBoard(board);
+                }
             };
         }
     },
     loadCards: function(boardId) {
-
           let boardCards = dataHandler.getCardsByBoardId(boardId);
           if (boardCards) {
               return boardCards;
