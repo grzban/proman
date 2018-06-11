7c66f759191000fe6f3737329d1639c5454db311
empyty <bartoszlistwan@gmail.com>
2018-06-11 10:56:41 +0200

    database connector added, started working on login

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
+
 app = Flask(__name__)
+app.secret_key = "codecoolSux"
 
 
 @app.route("/")
 def boards():
     ''' this is a one-pager which shows all the boards and cards '''
-    return render_template('boards.html')
+    if session["username"]:
+        username = session["username"]
+        return render_template("boards.html", username=username)
+    return render_template("boards.html")
+
+
+# @app.route("/login")
+# def login():
 
 
 def main():
     app.run(debug=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
