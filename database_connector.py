4d59881c6a0d23866c1c9911f8dde35b126cbf9b
gb <grzegorz.banaszek@gmail.com>
2018-06-13 20:16:27 +0200

    Deleted wireks db settings

--- database_connector.py
+++ database_connector.py
@@ -4,10 +4,10 @@ import psycopg2.extras
 
 
 def get_connection_string():
-    user_name = 'wirek'
-    password = 'norwida'
-    host = 'localhost'
-    database_name = 'wirek'
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
     env_variables_defined = user_name and password and host and database_name
 
