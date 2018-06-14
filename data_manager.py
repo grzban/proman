87a93838b15a66f7ad1e565eedec249e5a945c0e
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-06-14 04:19:07 -0700

    sql delete board command

--- data_manager.py
+++ data_manager.py
@@ -154,3 +154,10 @@ def delete_card(cursor, value):
                     WHERE id = %s
                     """, (value,))
     return None
+
+@database_connector.connection_handler
+def delete_board(cursor, value):
+    cursor.execute("""
+                    DELETE FROM boards
+                    WHERE id = %s""", (value,))
+    return None
